1f49f305d44f8959e8ca4ef4ee37c30aba31f474
Vitor Samuel Gonçalves Teixeira <00120190@aluno.uniso.br>
2025-11-03 13:56:05 -0300

    feat: adiciona logica de roles para o jwt context

--- backend/src/middleware/authTypo.js
+++ backend/src/middleware/authTypo.js
@@ -1,7 +1,6 @@
 export function authTypo(req, res, next) {
     if(req.user.tipoAcesso.trim().toLowerCase() !== "admin") {
         return res.status(403).json({ ok: false, error: "Acesso negado. Apenas administradores." });
-
     }
 
     next();
