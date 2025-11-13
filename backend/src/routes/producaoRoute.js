b37ef2d33825b84eeb0a20410c1a3b04b6ed576f
Vitor Samuel Gonçalves Teixeira <00120190@aluno.uniso.br>
2025-11-13 15:55:31 -0300

    fix: rota redundante

--- backend/src/routes/producaoRoute.js
+++ backend/src/routes/producaoRoute.js
@@ -9,7 +9,7 @@ export function createProducaoRoutes({ producaoService }) {
   router.post('/', authMiddleware, controller.produzir);
   router.get('/', authMiddleware, controller.listarProducoes);
   router.get('/produto/:produtoId', authMiddleware, controller.listarPorProduto);
-  router.get('/producao/lote/:lote', authMiddleware, controller.listarPorLote);
+  router.get('/lote/:lote', authMiddleware, controller.listarPorLote);
 
 
   return router;
